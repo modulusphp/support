be52946977ee170cf1975e92a434488ba17155e2
Donald Pakkies <donaldpakkies@gmail.com>
2018-11-24 16:43:36 +0200

    Feat: added a shortcut class for handling application shortcuts

---
+++ Shortcut.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modulus\Support;
+
+use Exception;
+use Modulus\Support\Exceptions\UndefinedShortcutException;
+use Modulus\Support\Exceptions\ShortcutsClassNotFoundException;
+
+class Shortcut
+{
+  /**
+   * Run helper
+   *
+   * @param string $function
+   * @param array $args
+   * @return mixed
+   */
+  public function run(string $function, array $args = [])
+  {
+    if (!class_exists(\App\Utils\Shortcuts::class)) {
+      throw new ShortcutsClassNotFoundException;
+    }
+
+    $shortcuts = new \App\Utils\Shortcuts;
+
+    if (method_exists($shortcuts, $function)) {
+      return call_user_func_array([$shortcuts, $function], $args);
+    }
+
+    throw new UndefinedShortcutException("Shortcut '{$function}()' does not exist", $args);
+  }
+}
